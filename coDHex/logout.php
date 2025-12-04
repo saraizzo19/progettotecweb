1324c9fa80ca32bb9dcdb3deda4ed029dbeaefb2
saraizzo19 <s.izzo8@studenti.unior.it>
2025-12-04 20:00:44 +0100

    Commentato logout.php

--- coDHex/logout.php
+++ coDHex/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-session_destroy(); // Distrugge tutti i dati della sessione
-header("Location: index.html"); // Rimanda alla home/login
+session_destroy(); // Distrugge tutti i dati della sessione. Avevamo riscontrato un problema riguardo le sessioni non chiuse
+header("Location: index.html"); // Rimanda alla landing page
 exit;
 ?>

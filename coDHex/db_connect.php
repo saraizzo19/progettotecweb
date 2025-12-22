ea5c7d712eb7f0a43e0ee1b17daf2263cbb8a96d
saraizzo19 <s.izzo8@studenti.unior.it>
2025-12-22 09:53:35 +0100

    db_connect.php commentato meglio

--- coDHex/db_connect.php
+++ coDHex/db_connect.php
@@ -1,12 +1,14 @@
 <?php
-// Codice per la creazione della connessione con il db
+// Codice per la creazione della connessione con il db, configurando parametri
 $host = 'localhost';
 $db   = 'codhex';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
 
+//Costruzione del DSN (data source name), specifica mysql, host e nome del db con il set di caratteri
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+//Attiva segnalazione di errori, importa fetch come array associativo, disabilita emulazione degli statement preparati, usando i nativi
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -14,8 +16,9 @@ $options = [
 ];
 
 try {
+// apre la connessione creando un'istanza. In caso di errore, cattura l'eccezione e la rilancia
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-    // In produzione non dovresti mostrare l'errore completo all'utente, ma per ora va bene
+
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
